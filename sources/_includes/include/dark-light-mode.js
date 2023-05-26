de5dd9ff7629b3ccca2d3bd875cf99d70be29c73
PierreTL <pierre.tampon@gmail.com>
2023-05-26 17:21:50 +0200

    Refonte du changement de mode claire/sombre

--- sources/_includes/include/dark-light-mode.js
+++ sources/_includes/include/dark-light-mode.js
@@ -1,4 +1,4 @@
-const toggleSwitch = document.querySelector(".js--switch");
+const toggleMode = document.querySelector(".js--toggleMode");
 const labelClair = document.querySelector(".js--switch-clair");
 const labelSombre = document.querySelector(".js--switch-sombre");
 const currentMode = localStorage.getItem('mode');
@@ -6,7 +6,7 @@ const currentMode = localStorage.getItem('mode');
 if (currentMode){
     document.documentElement.setAttribute('data-mode', currentMode);
     if (currentMode === 'light') {
-        toggleSwitch.checked = true;
+        toggleMode.setAttribute('aria-pressed', 'true');
         labelClair.classList.add('hide');
         labelSombre.classList.remove('hide');
     } else {
@@ -15,18 +15,20 @@ if (currentMode){
     }
 }
 
-function switchMode(e) {
-    if (e.target.checked) {
+toggleMode.addEventListener('click', function () {
+
+    if (this.getAttribute('aria-pressed') === 'false') {
         document.documentElement.setAttribute('data-mode', 'light');
         localStorage.setItem('mode', 'light');
         labelClair.classList.add('hide');
         labelSombre.classList.remove('hide');
+        this.setAttribute('aria-pressed', 'true');
     }else{
         document.documentElement.setAttribute('data-mode', 'dark');
         localStorage.setItem('mode', 'dark');
         labelClair.classList.remove('hide');
         labelSombre.classList.add('hide');
+        this.setAttribute('aria-pressed', 'false');
     }
-}
 
-toggleSwitch.addEventListener('change', switchMode, false);
+})
